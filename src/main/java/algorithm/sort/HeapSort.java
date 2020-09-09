72d64af7321e6640d33b46cdd2701b3b18ddf98e
zhijingzhuo <872567311@qq.com>
2020-09-09 20:49:04 +0800

    提交

--- src/main/java/algorithm/sort/HeapSort.java
+++ src/main/java/algorithm/sort/HeapSort.java
@@ -4,12 +4,19 @@ import com.alibaba.fastjson.JSONArray;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 堆排序
+ *
+ * 堆排序是一种选择排序，整体主要由【构建初始堆】+【交换堆顶元素和末尾元素并重建堆】两部分组成。
+ * 其中构建初始堆经推导复杂度为O(n)，
+ * 在交换并重建堆的过程中，需交换n-1次，而重建堆的过程中，
+ * 根据完全二叉树的性质，[log2(n-1),log2(n-2)...1]逐步递减，近似为nlogn。
+ * 所以堆排序时间复杂度一般认为就是O(nlogn)级。
  *
  * 比较各自的优劣、各种算法的思想及其使用场景。还有要会分析算法的时间和空间复杂度
  * 最佳情况：T(n) = O(nlogn)
  * 最差情况：T(n) = O(nlogn)
  * 平均情况：T(n) = O(nlogn)
+ *
+ * 不稳定排序
  *
  * @author zhijingzhuo
  * @since 2020/09/06 16:10
@@ -43,27 +50,27 @@ public class HeapSort {
 
     /**
      * 调整大顶堆（仅是调整过程，建立在大顶堆已构建的基础上）
+     * [比较 实质上是比较每个节点、子节点中的最大节点比较（下面通过index 移动来指向子节点中的最大值）]
      * @param arr
      * @param i
      * @param len
      */
     private static void adjustHeap(int[] arr, int i, int len) {
 
-        int tmp = arr[i];
-
+        int tmp = arr[i];//先取出当前元素i
         for(int k = 2*i+1;k<len;k=2*k+1){
-            if(k+1<len && arr[k]<arr[k+1]){
+
+            if(k+1<len && arr[k]<=arr[k+1]){//如果左子结点小于右子结点，k指向右子结点
                 k++;
             }
-
-            if(arr[k]>tmp){
+            if(arr[k]>=tmp){//如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
                 arr[i] = arr[k];
                 i = k;
             }else{
                 break;
             }
         }
-        arr[i] = tmp;
+        arr[i] = tmp;//将temp值放到最终的位置
     }
 
     // 交换元素
