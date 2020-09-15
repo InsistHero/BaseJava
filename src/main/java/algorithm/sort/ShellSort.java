61650011c55acb1e36d3ad843aeaf6999a0bba8a
zhijingzhuo <872567311@qq.com>
2020-09-15 16:20:41 +0800

    提交

--- src/main/java/algorithm/sort/ShellSort.java
+++ src/main/java/algorithm/sort/ShellSort.java
@@ -1,6 +1,7 @@
 package algorithm.sort;
 
 
+import com.alibaba.fastjson.JSONArray;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
  * 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；
  * 随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止。
  *
+ * https://www.cnblogs.com/chengxiao/p/6104371.html
+ *
  * 比较各自的优劣、各种算法的思想及其使用场景。还有要会分析算法的时间和空间复杂度
  *
  * @author zhijingzhuo
@@ -16,5 +19,39 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ShellSort {
+
+
+    private static int[] shellSort(int[] arr) {
+        if(arr == null || arr.length == 0){
+            return arr;
+        }
+
+        //增量gap，并逐步缩小增量
+        for(int gap=arr.length/2;gap>0;gap/=2){
+            //从第gap个元素，逐个对其所在组进行直接插入排序操作
+            for(int i=gap;i<arr.length;i++){
+                int j = i;
+                while(j-gap>=0 && arr[j]<arr[j-gap]){
+                    //插入排序采用交换法
+                    swap(arr,j,j-gap);
+                    j-=gap;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int j, int i) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {9,10,11,1,5,6,3};
+        a = shellSort(a);
+        log.info("a:{}", JSONArray.toJSONString(a));
+    }
+
 
 }
