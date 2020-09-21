b49bcf254359eb6d94400bbade509bcc72092654
zhijingzhuo <872567311@qq.com>
2020-09-21 20:42:34 +0800

    提交

---
+++ src/main/java/algorithm/leetcode0_60/Solution_3.java
@@ -0,0 +1,30 @@
+package algorithm.leetcode0_60;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ */
+@Slf4j
+public class Solution_3 {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+}
