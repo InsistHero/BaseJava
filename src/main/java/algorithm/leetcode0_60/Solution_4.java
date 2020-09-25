ee63a98f287a9273519c710b32c7c87fdce6489c
InsistHero <872567311@qq.com>
2020-09-25 18:48:16 +0800

    回文

--- src/main/java/algorithm/leetcode0_60/Solution_4.java
+++ src/main/java/algorithm/leetcode0_60/Solution_4.java
@@ -22,12 +22,35 @@ import java.util.HashSet;
 @Slf4j
 public class Solution_4 {
 
-    public String longestPalindrome(String s) {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
 
-        return null;
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
     }
 
     public static void main(String[] args) {
-
+        String s = "babad";
+        String result = longestPalindrome(s);
+        log.info("result:{}",result);
     }
+
 }
